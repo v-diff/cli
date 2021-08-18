24a50dd37145c3bbf49f0f1187e9a0d3d6a42b7a
Sunny Rekhi <rohit.rekhi@gmail.com>
2021-08-17 20:03:39 -0700

    Add zip functionality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os 
 import click 
 import subprocess
+import getpass 
+import shutil 
 
 # @click.group() works just like @click.command() except @click.group() can have subcommands 
 @click.command()
@@ -12,13 +15,34 @@ def cli(cmd):
     '''
     run_custom_build = cmd and cmd[0] == 'build'
     if run_custom_build:
-        return run_custom_build_logic()
+        return run_custom_build_logic(cmd)
     
     return fallback_to_docker(cmd)
 
-def run_custom_build_logic():
+def run_custom_build_logic(args):
     # check if email is in ~/.vdiff/config.json, if not, prompt for corporate email address and write it to that file.
-    click.echo('runing custom build logic')
+    # if not os.path.exists("~/.vdiff/config.json"):
+    #     # prompt user 
+    #     email = click.prompt(
+    #         "Enter your corporate email address",
+    #     )
+    #     os.mkdir("/.vdiff")
+    #     f = open("~/.vdiff/config.json", "x")
+    #     f.write(email)
+    #     f.close()
+    username = ''
+    try:
+        username = getpass.getuser()
+    except:
+        username = 'not_found'
+
+    # make a POST request to our server
+    args = list(args)
+    zipped = shutil.make_archive('docker_dir', 'tar')
+    # send it 
+    
+    # delete docker_dir locally
+
 
 def fallback_to_docker(cmd):
     subprocess.call("docker " + ' '.join(cmd), shell=True)
